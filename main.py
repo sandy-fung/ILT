ec4ab90e8f56c41e7f344e4e1cb47542bba1fac8
sandy.fang <sandy.fang@algoltek.com.tw>
2025-06-25 14:05:03 +0800

    debug

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from Controler import Controller
+from Controller import Controller
 from ILT_UI import UI
 
 if __name__ == "__main__":
     def dispatcher(event_type, event_data):
-        Controller.handle_event(event_type, event_data)
+        controller.handle_event(event_type, event_data)
 
     ui = UI(dispatcher)
     controller = Controller(ui)
